5df64547d8a2cc7ab7c65a354dbbc86804e6737b
kepo402 <alawodeolayinka2@gmail.com>
2024-07-20 20:39:25 +0100

    new commit

--- movies/models.py
+++ movies/models.py
@@ -1,12 +1,11 @@
-# movies/models.py
 from django.db import models
 
 class Content(models.Model):
     TYPE_CHOICES = [
         ('movie', 'Movie'),
         ('series', 'Series'),
-        ('nollywood', 'Nollywood'),  # Add Nollywood type
-        ('music', 'Music'),          # Add Music type
+        ('nollywood', 'Nollywood'),
+        ('music', 'Music'),
     ]
 
     title = models.CharField(max_length=200)
@@ -14,9 +13,11 @@ class Content(models.Model):
     type = models.CharField(max_length=50, choices=TYPE_CHOICES)
     details = models.TextField(null=True, blank=True)
     poster_url = models.URLField(max_length=300, null=True, blank=True)
+    subtitle_url = models.URLField(max_length=300, null=True, blank=True)
 
     def __str__(self):
         return self.title
+
 
 
 
