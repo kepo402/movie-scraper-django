cff1d6e05a2a92eec8abfabb655ddc0f8ee1248b
kepo402 <alawodeolayinka2@gmail.com>
2024-07-22 12:21:53 +0100

    new commit

--- movies/models.py
+++ movies/models.py
@@ -1,4 +1,32 @@
+# models.py
+import requests
 from django.db import models
+
+def get_new_download_link(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Cache-Control': 'max-age=0',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Origin': 'https://www.lulacloud.com',
+        'Referer': url,
+        'Sec-Ch-Ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Sec-Ch-Ua-Platform': '"Windows"',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'same-origin',
+        'Upgrade-Insecure-Requests': '1',
+    }
+
+    response = requests.post(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code == 302:
+        return response.headers.get('Location')
+    else:
+        return None
 
 class Content(models.Model):
     TYPE_CHOICES = [
@@ -14,6 +42,13 @@ class Content(models.Model):
     details = models.TextField(null=True, blank=True)
     poster_url = models.URLField(max_length=300, null=True, blank=True)
     subtitle_url = models.URLField(max_length=300, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.download_link:
+            new_download_link = get_new_download_link(self.download_link)
+            if new_download_link:
+                self.download_link = new_download_link
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
